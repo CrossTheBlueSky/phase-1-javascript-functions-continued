62fb9815244513a17a18046f3d4b0ca9c0d536d1
Derek Mercedes <mercedes.derek@gmail.com>
2023-10-29 17:37:30 -0600

    gives your task and encourages you

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(act = "roller-skate"){
+
+    return `This Saturday, I want to ${act}!`
+
+    
+}
+
+function mondayWork(act = "go to the office"){
+    return `This Monday, I will ${act}.`
+
+}
+
+function wrapAdjective(high = ''){
+    const out1 = "You are "
+    return function (adj = "special"){
+        const out2 = `${high}${adj}${high}!`
+        return  out1 + out2
+    }
+
+}
